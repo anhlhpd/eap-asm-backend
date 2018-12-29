6a089ef0a18f6424376c25348d80548102bd71e3
Tra <trauvmd00621@fpt.edu.vn>
2018-12-29 18:33:12 +0700

    split staff and student login api

--- Backend/Controllers/AuthenticationController.cs
+++ Backend/Controllers/AuthenticationController.cs
@@ -27,18 +27,17 @@ namespace Backend.Controllers
             _context = context;
         }
 
-        // POST: api/Authentication/Login
-        [Route("Login")]
+        // POST: api/Authentication/StudentLogin
+        [Route("StudentLogin")]
         [HttpPost]
-        public async Task<IActionResult> Login(LoginInformation loginInformation)
+        public async Task<IActionResult> StudentLogin(LoginInformation loginInformation)
         {
             // find 1 account with matching username in Account
             var ac = await _context.Account.SingleOrDefaultAsync(a =>
                     a.Username == loginInformation.Username);
             if (ac != null)
             {
-                // verify clientId to be either STU or TCH first
-                var isCorrectClient = ac.AccountId.StartsWith(loginInformation.ClientId, true, new CultureInfo("en-US"));
+                var isCorrectClient = ac.AccountId.StartsWith("STU");
                 if (isCorrectClient)
                 {
                     // check matching password
@@ -49,9 +48,11 @@ namespace Backend.Controllers
                             c.AccountId == ac.AccountId);
                         if (cr != null) // if account has never logged in
                         {
-                            // save token
                             var accessToken = TokenHandle.GetInstance().GenerateToken();
                             cr.AccessToken = accessToken;
+                            // save token
+                            _context.Credential.Update(cr);
+                            await _context.SaveChangesAsync();
                             return Ok(accessToken);
                         }
                         else
@@ -70,6 +71,59 @@ namespace Backend.Controllers
                     }
                     return NotFound("Password wrong; ogpw: " + ac.Password 
                         + " - newpw: " + PasswordHandle.GetInstance().EncryptPassword(loginInformation.Password, ac.Salt) 
+                        + " - salt: " + ac.Salt
+                        + " - inputpw: " + loginInformation.Password);
+                }
+                return BadRequest("Client wrong: " + loginInformation.ClientId + " og id: " + ac.AccountId);
+            }
+            return NotFound("Username wrong: " + loginInformation.Username);
+        }
+
+        [Route("StaffLogin")]
+        [HttpPost]
+        public async Task<IActionResult> StaffLogin(LoginInformation loginInformation)
+        {
+            // find 1 account with matching username in Account
+            var ac = await _context.Account.SingleOrDefaultAsync(a =>
+                    a.Username == loginInformation.Username);
+            if (ac != null)
+            {
+                var isManager = ac.AccountId.StartsWith("MNG");
+                var isAdmin = ac.AccountId.StartsWith("ADM");
+                if (isManager || isAdmin)
+                {
+                    var roles = _context.AccountRoles.Where(acr => acr.AccountId == ac.AccountId);
+                    // check matching password
+                    if (ac.Password == PasswordHandle.GetInstance().EncryptPassword(loginInformation.Password, ac.Salt))
+                    {
+                        // check if account is logged in elsewhere
+                        var cr = await _context.Credential.SingleOrDefaultAsync(c =>
+                            c.AccountId == ac.AccountId);
+                        if (cr != null) // if account has never logged in
+                        {
+                            var accessToken = TokenHandle.GetInstance().GenerateToken();
+                            cr.AccessToken = accessToken;
+                            // save token
+                            _context.Credential.Update(cr);
+                            await _context.SaveChangesAsync();
+                            return Ok(accessToken);
+                        }
+                        else
+                        {
+                            // create new credential with AccountId
+                            var firstCredential = new Credential
+                            {
+                                AccountId = ac.AccountId,
+                                AccessToken = TokenHandle.GetInstance().GenerateToken()
+                            };
+                            _context.Credential.Add(firstCredential);
+                            await _context.SaveChangesAsync();
+                            // save token
+                            return Ok(TokenHandle.GetInstance().GenerateToken());
+                        }
+                    }
+                    return NotFound("Password wrong; ogpw: " + ac.Password
+                        + " - newpw: " + PasswordHandle.GetInstance().EncryptPassword(loginInformation.Password, ac.Salt)
                         + " - salt: " + ac.Salt
                         + " - inputpw: " + loginInformation.Password);
                 }
