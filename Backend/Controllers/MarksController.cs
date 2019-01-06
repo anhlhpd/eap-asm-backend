0bdaf4df561c7e7b302e4f8aaa97e3a5e699eea3
nguyenhoang24695 <hoangnvd00536@fpt.edu.vn>
2019-01-06 14:21:29 +0700

    add get mark

--- Backend/Controllers/MarksController.cs
+++ Backend/Controllers/MarksController.cs
@@ -24,213 +24,69 @@ namespace Backend.Controllers
         // Get all marks in 1 subject
         // GET: api/Marks/
         [Route("student")]
-        [HttpGet("{subjectId}")]
-        public async Task<IActionResult> StudentGetMarksOneSubject(string subjectId)
+        [HttpGet]
+        public async Task<IActionResult> StudentGetMarks()
         {
             string tokenHeader = Request.Headers["Authorization"];
             var token = tokenHeader.Replace("Basic ", "");
-            var cr =  _context.Credential.SingleOrDefault(c =>
-                    c.AccessToken == token);
+            var cr = _context.Credential.SingleOrDefault(c =>
+                c.AccessToken == token);
             var accountRoles = _context.AccountRoles.Where(ar => ar.AccountId == cr.OwnerId).Include(ar => ar.Role);
-            if (accountRoles.SingleOrDefault(ar=>ar.Role.Name == "Student") != null) //Nếu là học sinh thì trả về điểm môn này của nó
+            if (Request.Query.ContainsKey("SubjectId"))
             {
-                _context.Mark.Where(m=>m.)
-                return  Ok()
+                var subjectId = Request.Query["SubjectId"].ToString();
+                return Ok(_context.Mark.Where(m => m.AccountId == cr.OwnerId && m.SubjectId == subjectId)
+                    .Include(m => m.Subject));
             }
-            //return _context.Mark.Where(m => m.AccountId == cr.OwnerId && m.SubjectId == subjectId);
+
+            return Ok(_context.Mark.Where(m => m.AccountId == cr.OwnerId).Include(m => m.Subject));
+
         }
 
-        // Student: get all marks in all subject
-        // GET: api/Marks/StudentGetMarksAllSubject
-        //[HttpGet("StudentGetMarksAllSubject")]
-        //public IEnumerable<Mark> StudentGetMarksAllSubject(HttpContext context)
-        //{
-        //    // Call method StudentGetAllSubject from SubjectController ?
-        //    SubjectsController ctrl = new SubjectsController();
-        //    ctrl.ControllerContext = ControllerContext;
-        //    IEnumerable<ClazzSubject> listClazzSubjects = ctrl.StudentGetAllSubject(context);
-        //    IEnumerable<Mark> listMarks = null;
-        //    foreach (var clazzSubject in listClazzSubjects)
-        //    {
-        //        IEnumerable<Mark> listAllMarksOneSubject = StudentGetMarksOneSubject(clazzSubject.SubjectId, context);
-        //        foreach(var mark in listAllMarksOneSubject)
-        //        {
-        //            listMarks.Append(mark);
-        //        }
-        //    }
-        //    return listMarks;
-        //}
-
-        // Manager: get all marks in 1 subject from 1 student
-        // GET: api/Marks/ManagerGetMarksOneSubjectOneStudent
-        [HttpGet("Manager/GetMarksOneSubjectOneStudent")]
-        public IEnumerable<Mark> ManagerGetMarksOneSubjectOneStudent(string subjectId, string studentId)
-        {
-            return _context.Mark.Where(m => m.AccountId == studentId && m.SubjectId == subjectId);
-        }
-
-        // Manager: get all marks in all subject from 1 student
-        // GET: api/Marks/ManagerGetMarksAllSubjectOneStudent
-        //[HttpGet("Manager/GetMarksAllSubjectOneStudent")]
-        //public IEnumerable<Mark> ManagerGetMarksAllSubjectOneStudent(string studentId)
-        //{
-        //    // Call method StudentGetAllSubject from SubjectController ?
-        //    SubjectsController ctrl = new SubjectsController();
-        //    ctrl.ControllerContext = ControllerContext;
-        //    IEnumerable<ClazzSubject> listClazzSubjects = ctrl.ManagerGetAllSubjectOneStudent(studentId);
-        //    IEnumerable<Mark> listMarks = null;
-        //    foreach (var clazzSubject in listClazzSubjects)
-        //    {
-        //        IEnumerable<Mark> listAllMarksOneSubject = ManagerGetMarksOneSubjectOneStudent(clazzSubject.SubjectId, studentId);
-        //        foreach (var mark in listAllMarksOneSubject)
-        //        {
-        //            listMarks.Append(mark);
-        //        }
-        //    }
-        //    return listMarks;
-        //}
-
-        // Manager: get all marks in 1 subject from all student
-        // GET: api/Marks/GetMarksOneSubjectAllStudent
-        [HttpGet("Manager/GetMarksOneSubjectAllStudent")]
-        public IEnumerable<Mark> ManagerGetMarksOneSubjectAllStudent(string subjectId)
-        {
-            // Call method ManagerGetAllStudentOneSubject from ClazzAccountsController ?
-            ClazzAccountsController ctrl = new ClazzAccountsController();
-            ctrl.ControllerContext = ControllerContext;
-            IEnumerable<ClazzAccount> listClazzAccounts = ctrl.ManagerGetAllStudentOneSubject(subjectId);
-            IEnumerable<Mark> listMarks = null;
-            foreach (var clazzAccount in listClazzAccounts)
-            {
-                IEnumerable<Mark> listAllMarksOneStudent = ManagerGetMarksOneSubjectOneStudent(subjectId, clazzAccount.AccountId);
-                foreach (var mark in listAllMarksOneStudent)
-                {
-                    listMarks.Append(mark);
-                }
-            }
-            return listMarks;
-        }
-
-        // Manager: get all marks in 1 subject from all student in 1 class
-        // GET: api/Marks/GetMarksOneSubjectAllStudentOneClazz
-        [HttpGet("Manager/GetMarksOneSubjectAllStudentOneClazz")]
-        public IEnumerable<Mark> ManagerGetMarksOneSubjectAllStudentOneClazz(string subjectId, string clazzId)
-        {
-            // Call method ManagerGetAllStudentOneSubject from ClazzAccountsController ?
-            ClazzAccountsController ctrl = new ClazzAccountsController();
-            ctrl.ControllerContext = ControllerContext;
-            IEnumerable<ClazzAccount> listClazzAccounts = ctrl.ManagerGetAllStudentOneClazz(clazzId);
-            IEnumerable<Mark> listMarks = null;
-            foreach (var clazzAccount in listClazzAccounts)
-            {
-                IEnumerable<Mark> listAllMarksOneStudent = ManagerGetMarksOneSubjectOneStudent(subjectId, clazzAccount.AccountId);
-                foreach (var mark in listAllMarksOneStudent)
-                {
-                    listMarks.Append(mark);
-                }
-            }
-            return listMarks;
-        }
-        // GET: api/Marks
+        [Route("Manager")]
         [HttpGet]
-        public IEnumerable<Mark> GetMark()
+        public async Task<IActionResult> ManagerGetMarks()
         {
-            return _context.Mark;
-        }
-
-        // GET: api/Marks/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetMark([FromRoute] long id)
-        {
-            if (!ModelState.IsValid)
+            bool isValid = (Request.Query.ContainsKey("ClassId") == Request.Query.ContainsKey("SubjectId")) &&
+                           (Request.Query.ContainsKey("SubjectId") != Request.Query.ContainsKey("StudentId"));
+            if (isValid)
             {
-                return BadRequest(ModelState);
-            }
-
-            var mark = await _context.Mark.FindAsync(id);
-
-            if (mark == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(mark);
-        }
-
-        // PUT: api/Marks/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMark([FromRoute] long id, [FromBody] Mark mark)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != mark.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(mark).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MarkExists(id))
+                if (Request.Query.ContainsKey("ClassId") && Request.Query.ContainsKey("SubjectId"))
                 {
+                    var classId = Request.Query["ClassId"].ToString();
+                    var subjectId = Request.Query["SubjectId"].ToString();
+                    var classAccounts = _context.ClazzAccount.Where(ca => ca.ClazzId == classId);
+
+                    List<Mark> listMarks = new List<Mark>();
+                    if (classAccounts.Any())
+                    {
+                        foreach (var classAccount in classAccounts)
+                        {
+                            var markAccountList = _context.Mark.Include(m => m.Subject).Include(m => m.Account)
+                                .Where(m => m.AccountId == classAccount.AccountId && m.SubjectId == subjectId);
+                            foreach (var mark in markAccountList)
+                            {
+                                listMarks.Add(mark);
+                            }
+                        }
+
+                        return Ok(listMarks);
+                    }
+
                     return NotFound();
                 }
-                else
+
+                if (Request.Query.ContainsKey("StudentId"))
                 {
-                    throw;
+                    var studentId = Request.Query["StudentId"].ToString();
+                    return Ok(_context.Mark.Where(m => m.AccountId == studentId).Include(m => m.Subject));
                 }
-            }
 
-            return NoContent();
-        }
-
-        // POST: api/Marks
-        [HttpPost]
-        public async Task<IActionResult> PostMark([FromBody] Mark mark)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Mark.Add(mark);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMark", new { id = mark.Id }, mark);
-        }
-
-        // DELETE: api/Marks/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMark([FromRoute] long id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var mark = await _context.Mark.FindAsync(id);
-            if (mark == null)
-            {
                 return NotFound();
             }
 
-            _context.Mark.Remove(mark);
-            await _context.SaveChangesAsync();
-
-            return Ok(mark);
+            return BadRequest();
         }
 
-        private bool MarkExists(long id)
-        {
-            return _context.Mark.Any(e => e.Id == id);
-        }
     }
 }
